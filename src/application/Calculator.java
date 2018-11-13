d3e478e27a0c8fe3b937427b8a16b17dc97837a0
adrian.obermuehlner <aobermuhlner@gmail>
2018-11-13 21:29:59 +0100

    calculate calculate function implemented, which will recongize which fields it does have and which not and so it will use the right function

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -1,15 +1,18 @@
 package application;
 
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
 /**
  * Berechnet das Formelrad
- * @author Peter Rutschmann
- * @version 13.09.2018
+ * @author Adrian Obermühlner
  */
 public class Calculator {
-	private double leistung;
-	private double spannung;
-	private double strom;
-	private double widerstand;
+	private Double leistung;
+	private Double spannung;
+	private Double strom;
+	private Double widerstand;
 	
 	public Calculator(double leistung, double spannung, double strom, double widerstand) {
 		super();
@@ -43,17 +46,44 @@ public class Calculator {
 				", widerstand="	+ widerstand + "]";
 	}
 
-	public void calculate() {
-            
-		
-                
-                
-	}
+	public boolean calculate() {
+        long filledFieldsAmount = Stream.of(leistung, spannung, strom, widerstand)
+                .filter(Objects::nonNull)
+                .count();
+
+		if (leistung != null && spannung != null) {
+			strom = iAusPundU(spannung, leistung);
+			widerstand = rAusUundP(spannung, leistung);
+		} else if (leistung != null && strom != null) {
+			spannung = uAusPUndI(leistung, strom);
+			widerstand = rAusPundI(leistung, strom);
+		} else if (leistung != null && widerstand != null) {
+			spannung = uAusPundR(leistung, widerstand);
+			strom = iAusRundU(widerstand, leistung);
+		} else if (spannung != null && strom != null) {
+			leistung = pAusUundI(spannung, strom);
+			widerstand = rAusUundI(spannung, strom);
+		} else if (spannung != null && widerstand != null) {
+			leistung = pAusUundR(spannung, widerstand);
+			strom = iAusRundU(widerstand, spannung);
+		} else if (strom != null && widerstand != null) {
+			leistung = pAusIundR(strom, widerstand);
+			spannung = uAusRundI(widerstand, strom);
+		} else {
+			throw new RuntimeException("Not enough args");
+		}
+
+        return filledFieldsAmount < 3;
+    }
 	
 	/* Hier die Methoden mit den Formlen hinzuf�gen
 	 */
 
-	 
+	public double iAusPundU(double p, double u){
+            double result = p/u;
+            System.out.println("The result of ampere from volt (" + u + ") and watt (" + p + ") is " + result);
+             return result;
+        } 
         public double iAusPundR(double p, double r){
             double result = java.lang.Math.sqrt((p/r));
             System.out.println("The result of ampere from ohm (" + r + ") and watt (" + p + ") is " + result);
@@ -82,21 +112,38 @@ public class Calculator {
         return result;
         }
 
-	private double uFromRandI(double r, double i) {
+	private double uAusRundI(double r, double i) {
 		double result = r * i;
 		System.out.println("Result of volt from ohm (" + r + ") and ampere (" + i + ") is " + result);
 		return result;
 	}
 
-	private double UFromPAndI(double p, double i) {
+	private double uAusPUndI(double p, double i) {
 		double result = p / i;
 		System.out.println("Result of volt from watt (" + p + ") and ampere (" + i + ") is " + result);
 		return result;
 	}
 
-	private double UFromPAndR(double p, double r) {
+	private double uAusPundR(double p, double r) {
 		double result = Math.sqrt(p * r);
 		System.out.println("Result of volt from watt (" + p + ") and ohm (" + r + ") is " + result);
 		return result;
 	}
+        //Ohm
+        private double rAusUundI(double u, double i) {
+		double result = u/i;
+        System.out.println("Result of ohm from volt (" + u + ") and ampere (" + i + ") is " + result);
+        return result;
+	}
+        private double rAusPundI(double p, double i) {
+		double result =  p/Math.pow(i, 2);
+        System.out.println("Result of ohm from watt (" + p + ") and ampere (" + i + ") is " + result);
+        return result;
+    }
+
+	private double rAusUundP(double u, double p) {
+		double result = Math.pow(u, 2)/p;
+        System.out.println("Result of ohm from volt (" + u + ") and watt (" + p + ") is " + result);
+        return result;
+    }
 }
